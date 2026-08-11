043a83cd426b3e5ad4b49223564b92630e30ad18
Tiago <tlopes057p@gmail.com>
2026-08-11 02:37:05 +0100

    increase font size. change label color

--- Source/Components/Pages/Mining.lua
+++ Source/Components/Pages/Mining.lua
@@ -10,22 +10,33 @@ function Mining:Build(parent)
 
     local charSkillLbl = AceGUI:Create("Label")
     charSkillLbl:SetFullWidth(true)
+    charSkillLbl:SetFontObject(GameFontHighlightLarge)
     local miningSkill = Functions_Professions:GetProfessionSkillNumber("Mining")
     local maxMiningSkill = Functions_Professions:GetProfessionMaxSkillNumber("Mining")
 
     local charSkill = "Current Mining Skill: "
     if miningSkill then
         charSkill = charSkill .. miningSkill .. "/".. maxMiningSkill
-        
+
         local shouldGoTrain = Functions_Professions:GetProfessionShouldGoLearn(miningSkill, maxMiningSkill, "Mining")
         if (shouldGoTrain) then
             charSkill = charSkill .. " You should go train soon!"
+            charSkillLbl:SetColor(1, 0, 0)
+        else
+            charSkillLbl:SetColor()
         end
     else
         charSkill = charSkill .. "N/A"
+        charSkillLbl:SetColor()
     end
     charSkillLbl:SetText(charSkill)
     scroll:AddChild(charSkillLbl)
+
+    -- Spacer so the table doesn't sit flush against the label above it.
+    local spacer = AceGUI:Create("Label")
+    spacer:SetFullWidth(true)
+    spacer:SetHeight(12)
+    scroll:AddChild(spacer)
 
     -- Node/skill table (data from MiningData.lua)
     local table = DataTable:Build(parent, MiningData)
