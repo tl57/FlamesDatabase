78bface83c6f627240defd4ef12191e83c6eb803
Tiago <tlopes057p@gmail.com>
2026-08-14 19:56:23 +0100

    refactor Changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,30 @@
+# Changelog
+
+## v0.1.4
+### Added
+- Added Dungeon Info tab.
+### Fixed
+- more UI bugs
+
+## v0.1.3
+### Added
+- Added support for keybinds function.
+- Added Dungeon Tab with Quests tab.
+### Fixed
+- more UI bugs
+
+## v0.1.2
+### Changed
+- Added Recommendations section in Professions.
+
+## v0.1.1
+### Added
+- Added Herbalism tab.
+- Add support for Settings page.
+### Fixed
+- Fixed several UI bugs with Mining tab.
+
+## v0.1.0
+### Added
+- Added Mining Tab.
+- Added Minimap Button.
---
+++ Source/Data/Changelogs/ChangelogData.lua
@@ -0,0 +1,42 @@
+-- ChangelogData
+-- Static changelog entries for the in-game changelog panel.
+-- Newest version first; consumed by changelogframe.lua.
+
+ChangelogData = {
+    {
+        version = "0.1.4",
+        changes = {
+            { category = "Added", text = "Added Dungeon Info tab." },
+            { category = "Fixed", text = "More UI bugs." },
+        },
+    },
+    {
+        version = "0.1.3",
+        changes = {
+            { category = "Added", text = "Added support for keybinds function." },
+            { category = "Added", text = "Added Dungeon Tab with Quests tab." },
+            { category = "Fixed", text = "More UI bugs." },
+        },
+    },
+    {
+        version = "0.1.2",
+        changes = {
+            { category = "Changed", text = "Added Recommendations section in Professions." },
+        },
+    },
+    {
+        version = "0.1.1",
+        changes = {
+            { category = "Added", text = "Added Herbalism tab." },
+            { category = "Added", text = "Added support for Settings page." },
+            { category = "Fixed", text = "Fixed several UI bugs with Mining tab." },
+        },
+    },
+    {
+        version = "0.1.0",
+        changes = {
+            { category = "Added", text = "Added Mining Tab." },
+            { category = "Added", text = "Added Minimap Button." },
+        },
+    },
+}
